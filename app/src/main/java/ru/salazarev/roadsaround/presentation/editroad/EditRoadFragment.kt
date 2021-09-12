2e42c136a47e57147f3bde4b73e843c53beefe06
Семён Лазарев <lazarevsemyonandreevich@gmail.com>
2021-09-12 20:55:08 +0300

    Карта, рабочее состояние.

--- app/src/main/java/ru/salazarev/roadsaround/presentation/editroad/EditRoadFragment.kt
+++ app/src/main/java/ru/salazarev/roadsaround/presentation/editroad/EditRoadFragment.kt
@@ -1,20 +1,48 @@
 package ru.salazarev.roadsaround.presentation.editroad
 
+import android.location.Location
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
 import ru.salazarev.roadsaround.App
 import ru.salazarev.roadsaround.R
 import ru.salazarev.roadsaround.databinding.FragmentEditRoadBinding
 import ru.salazarev.roadsaround.presentation.MainActivity
 import javax.inject.Inject
 
-class EditRoadFragment : Fragment() {
+
+class EditRoadFragment : Fragment(), OnMapReadyCallback {
+
+    enum class DirectionType(val type: String) {
+        Driving("driving"),
+        Walking("walking"),
+        Cycling("cycling")
+    }
+
+    companion object {
+        private const val REQUEST_CODE = 100
+        private const val DEFAULT_ZOOM = 15
+    }
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
+    private var lastKnownLocation: Location? = null
+    private lateinit var mMap: GoogleMap
+    private var currentPolyline: Polyline? = null
+    private val listMarker: MutableList<Marker> = mutableListOf()
 
     private var _binding: FragmentEditRoadBinding? = null
     private val binding get() = _binding!!
@@ -25,7 +53,8 @@ class EditRoadFragment : Fragment() {
     lateinit var editRoadViewModelFactory: EditRoadViewModelFactory
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
 
         _binding = FragmentEditRoadBinding.inflate(inflater, container, false)
 
@@ -33,6 +62,10 @@ class EditRoadFragment : Fragment() {
             .inject(this)
         viewModel =
             ViewModelProvider(this, editRoadViewModelFactory).get(EditRoadViewModel::class.java)
+
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         return binding.root
     }
@@ -56,7 +89,7 @@ class EditRoadFragment : Fragment() {
                 when (it.itemId) {
                     R.id.btn_complete_edit_road -> {
                         val bundle = Bundle().apply {
-                            putString("ROAD","POINTS")
+                            putString("ROAD", "POINTS")
                         }
                         (activity as MainActivity).navController
                             .navigate(R.id.action_editRoadFragment_to_editEventFragment, bundle)
@@ -72,6 +105,19 @@ class EditRoadFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
 
