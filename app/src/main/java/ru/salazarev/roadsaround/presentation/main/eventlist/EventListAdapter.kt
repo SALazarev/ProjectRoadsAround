b5d57c2e16071856f5582f564623d50c98f79ec8
Семён Лазарев <lazarevsemyonandreevich@gmail.com>
2021-09-17 13:37:02 +0300

    Работа над разметкой карточек инвентов.

--- app/src/main/java/ru/salazarev/roadsaround/presentation/main/eventlist/EventListAdapter.kt
+++ app/src/main/java/ru/salazarev/roadsaround/presentation/main/eventlist/EventListAdapter.kt
@@ -2,9 +2,11 @@ package ru.salazarev.roadsaround.presentation.main.eventlist
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import ru.salazarev.roadsaround.R
+import ru.salazarev.roadsaround.domain.event.EventInteractor
 import ru.salazarev.roadsaround.models.presentation.EventPreview
 import ru.salazarev.roadsaround.presentation.chat.messagelist.EventDiffUtilCallback
 
@@ -24,7 +26,23 @@ class EventListAdapter(
 
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
         val item = data[position]
-        holder.set(item)
+
+        val context = holder.itemView.context
+        val resource =  context.resources
+
+        val typeWork = when (item.typeWorkWithEvent){
+            EventInteractor.Companion.TypeWorkWithEvent.GUEST -> ""
+            EventInteractor.Companion.TypeWorkWithEvent.AUTHOR -> resource.getString(R.string.organizer)
+            EventInteractor.Companion.TypeWorkWithEvent.MEMBER -> resource.getString(R.string.member)
+        }
+
+        val backgroundColor = when (item.typeWorkWithEvent){
+            EventInteractor.Companion.TypeWorkWithEvent.GUEST ->  ContextCompat.getColor(holder.itemView.context,R.color.colorAccent)
+            EventInteractor.Companion.TypeWorkWithEvent.AUTHOR -> ContextCompat.getColor(holder.itemView.context,R.color.design_default_color_primary_dark)
+            EventInteractor.Companion.TypeWorkWithEvent.MEMBER -> ContextCompat.getColor(holder.itemView.context,R.color.design_default_color_primary)
+        }
+
+        holder.set(item,typeWork,backgroundColor)
         holder.setOnClick { callback.onClick(item.id, item.nameEvent, item.typeWorkWithEvent) }
     }
 
--- app/src/main/java/ru/salazarev/roadsaround/presentation/main/eventlist/EventViewHolder.kt
+++ app/src/main/java/ru/salazarev/roadsaround/presentation/main/eventlist/EventViewHolder.kt
@@ -2,18 +2,21 @@ package ru.salazarev.roadsaround.presentation.main.eventlist
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import ru.salazarev.roadsaround.R
 import ru.salazarev.roadsaround.databinding.ListItemEventBinding
 import ru.salazarev.roadsaround.models.presentation.EventPreview
 
 class EventViewHolder(item: View) : RecyclerView.ViewHolder(item) {
     private val binding: ListItemEventBinding = ListItemEventBinding.bind(item)
 
-    fun set(data: EventPreview) {
+    fun set(data: EventPreview, typeWork: String,backgroundColor: Int) {
         binding.apply {
             tvAuthor.text = data.authorName
             tvMotionType.text = data.motionType
             tvTimeEvent.text = data.time
             tvNameEvent.text = data.nameEvent
+            tvTypeParticipate.text = typeWork
+            rvItem.setCardBackgroundColor(backgroundColor)
         }
     }
 
